3654d12478f6240eac89c9c8ffef2819bda01499
Adriel Gama <adrielgama@gmail.com>
2021-05-28 15:16:09 -0300

    Delete src directory

--- src/controller/Auth/auth.js
+++
@@ -1,9 +0,0 @@
-export const TOKEN_AUTH = localStorage.getItem("token");
-
-export const isAuth = () => localStorage.getItem(TOKEN_AUTH) !== null;
-
-console.log(isAuth());
-
-export const login = (token) => {
-  localStorage.setItem(TOKEN_AUTH, token);
-};
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://guaibim.ajsy.com.br/api/",
-});
-
-export default api;
